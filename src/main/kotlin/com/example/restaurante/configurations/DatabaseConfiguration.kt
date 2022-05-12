f59476fef027f50165b30fbb873985e2981a09b7
Vandeilsonpp <vandeilson.nobre@picpay.com>
2022-05-12 00:26:37 -0300

    criado config de DB para heroku postgress

---
+++ src/main/kotlin/com/example/restaurante/configurations/DatabaseConfiguration.kt
@@ -0,0 +1,26 @@
+package com.example.restaurante.configurations
+
+import org.apache.commons.dbcp2.BasicDataSource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.net.URI
+import java.net.URISyntaxException
+
+@Configuration
+class DatabaseConfiguration {
+
+    @Bean
+    @Throws(URISyntaxException::class)
+    fun dataSource(): BasicDataSource {
+        val dbUri = URI(System.getenv("DATABASE_URL"))
+        val username = dbUri.userInfo.split(":").toTypedArray()[0]
+        val password = dbUri.userInfo.split(":").toTypedArray()[1]
+        val dbUrl = "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path + "?sslmode=require"
+
+        val basicDataSource = BasicDataSource()
+        basicDataSource.url = dbUrl
+        basicDataSource.username = username
+        basicDataSource.password = password
+        return basicDataSource
+    }
+}
